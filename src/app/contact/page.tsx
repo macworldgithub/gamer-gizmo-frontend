a9621f83df966952697b10a6867dfd9a5536c10d
Talal <talalzafar84@gmail.com>
2024-12-17 18:25:20 +0500

    .

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,16 @@
 import Banner from "@/components/Common/Banner";
 import Wrapper from "@/components/Common/Wrapper/Wrapper";
+import PageHeader from "@/components/PageHeader";
 import React from "react";
 
 const AboutPage = () => {
   return (
-    <Wrapper>
-      <div className="h-96 w-full bg-blue-950"></div>
-      <Banner />
-    </Wrapper>
+    <>
+      <PageHeader pageName={"Contact Us"} />
+      <Wrapper>
+        <div className="h-96 w-full bg-blue-950"></div>
+      </Wrapper>
+    </>
   );
 };
 
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,21 +1,29 @@
 import Image from "next/image";
 import React, { ReactNode } from "react";
+import Wrapper from "./Common/Wrapper/Wrapper";
 
 interface PageHeadProps {
-  pageName: string;
+  pageName?: string;
 }
 
 const PageHeader = ({ pageName }: PageHeadProps) => {
   return (
-    <div className="w-[100%] lg:h-[100px]  h-[30%] bg-custom-gradient text-[white] font-bold max-sm:flex max-sm:justify-center ">
-      <Image
-        src={"/images/HeroArea.png"}
-        width={0}
-        height={0}
-        sizes="100%"
-        style={{ width: "100%", height: "100%", objectFit: "fill" }}
-        alt="Hero Image"
-      />
+    <div className="relative">
+      <div className="w-[100%] lg:h-[250px]  h-[140px] bg-custom-gradient text-[white] font-bold max-sm:flex max-sm:justify-center ">
+        <Image
+          src={"/images/HeroArea.png"}
+          width={0}
+          height={0}
+          sizes="100%"
+          style={{ width: "100%", height: "100%", objectFit: "fill" }}
+          alt="Hero Image"
+        />
+      </div>
+      <Wrapper>
+        <div className="absolute  top-12 lg:top-24 text-3xl lg:text-5xl text-white font-bold">
+          <h1>{pageName}</h1>
+        </div>
+      </Wrapper>
     </div>
   );
 };
