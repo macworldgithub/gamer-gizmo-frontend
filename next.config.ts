55b90a125410f5ecde5706d8067fea9f6cb1f633
ayla-imran851 <ayla@macworldtechnology.com>
2025-01-01 11:36:06 +0500

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,26 @@
-// next.config.js
-const nextConfig = {
-  webpack(config: any) {
+import { NextConfig } from 'next'; // Import NextConfig type
+import { Configuration } from 'webpack'; // Import webpack's Configuration type
+
+const nextConfig: NextConfig = {
+  webpack(config: Configuration) {
     // Find and modify the rule that handles SVGs
-    const fileLoaderRule = config.module.rules.find(
-      (rule: any) => rule.test && rule.test.test(".svg")
+    const fileLoaderRule = config.module?.rules.find(
+      (rule) => rule.test && rule.test instanceof RegExp && rule.test.test('.svg')
     );
 
     // Exclude SVGs from the default file loader
     if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/;
+      (fileLoaderRule as any).exclude = /\.svg$/; // Type cast to avoid issues
     }
 
     // Add a new rule for handling SVGs as React components
-    config.module.rules.push({
+    config.module?.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
