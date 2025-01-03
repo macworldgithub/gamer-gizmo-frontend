85969414cb55e81db12edd386c2e8e898bdb667f
ayla <ayla@macworldtechnology.com>
2025-01-03 16:17:14 +0500

    nav items swapped

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -59,8 +59,8 @@ const BottomNavigationBar = () => {
       />
       {/* </div> */}
       <div className="hidden md:flex md:gap-5 md:pl-2 lg:gap-[2rem] font-bold md:text-[0.6rem]  lg:text-[0.8rem] whitespace-nowrap text-navTextLight dark:text-white">
-        <Link href="/console-screen">Laptops</Link>
         <Link href="/console-screen">Desktops</Link>
+        <Link href="/console-screen">Laptops</Link>
         <Link href="/console-screen">Store</Link>
         <Link href="/console-screen">Components</Link>
         <Link href="/console-screen">Blogs</Link>
