1c6d35783debb0f504c226b1c3154cadf140afe4
ayla-imran851 <ayla@macworldtechnology.com>
2025-01-11 00:30:13 +0500

    ..

--- src/components/PopularItemSection.tsx
+++ src/components/PopularItemSection.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Wrapper from "./Common/Wrapper/Wrapper";
 
 interface Product {
   id: number;
@@ -32,53 +33,58 @@ const PopularItemSection: React.FC<SectionProps> = ({
   };
 
   return (
-    <div className="relative w-full">
-      <div className="my-8 ">
-        {/* Title and Explore Button */}
-        <div className="flex justify-between max-sm:flex-col  max-sm:items-start mx-auto items-center mb-6">
-          <div>
-            <h2 className="text-2xl font-bold text-gray-900 dark:text-white max-sm:text-[1.1rem]">
-              {title}
-            </h2>
-            <p className="text-gray-500 dark:text-white mt-2 text-sm max-sm:text-[0.6rem]">
-              {subtitle}
-            </p>
-          </div>
-          <button
-            onClick={onExplore}
-            className="bg-custom-gradient text-white px-4 py-2 rounded-full text-sm flex md:justify-end hover:bg-purple-600 max-sm:mt-3"
-          >
-            Explore More
-          </button>
-        </div>
 
-        {/* Product Cards */}
-        <div className="grid max-sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-4">
+    <div >
+      <div className="my-8 ">
+        <Wrapper>
+          <div className="flex justify-between max-sm:flex-col  max-sm:items-start mx-auto items-center mb-6">
+            <div>
+              <h2 className="text-2xl font-bold text-gray-900 dark:text-white max-sm:text-[1.1rem]">
+                {title}
+              </h2>
+              <p className="text-gray-500 dark:text-white mt-2 md:text-sm sm:text-[0.8] max-sm:text-[0.6rem]">
+                {subtitle}
+              </p>
+            </div>
+            <button
+              onClick={onExplore}
+              className="bg-custom-gradient text-white px-4 py-2 rounded-full text-sm max-sm:text-[0.6rem] flex md:justify-end  hover:bg-purple-600 max-sm:mt-3"
+            >
+              Explore More
+            </button>
+          </div>
+        </Wrapper>
+       
+        <div className="flex gap-4 overflow-x-auto scrollbar-hide">
           {products.map((product) => (
             <div
               key={product.id}
-              className="dark:bg-black dark:text-white shadow-md rounded-lg overflow-hidden border border-gray-200"
+              className="flex-none dark:bg-black dark:text-white shadow-md rounded-lg overflow-hidden border border-gray-200 max-md:w-[40%]  md:w-[25%] lg:w-[20%]"
             >
-              {/* Product Image */}
-              <Image
-                src={product.imageUrl}
-                alt={product.name}
-                width={100}
-                height={100}
-                className="flex justify-center mx-auto mt-2 w-[12rem] h-[8rem] px-5"
-              />
+              {/* Main Content */}
+              <div className="relative w-full h-[12rem] max-md:h-[10rem] max-sm:h-[8rem]">
+                {/* Product Image */}
+                <Image
+                  src={product.imageUrl}
+                  alt={product.name}
+                  layout="fill"
+                  objectFit="cover"
+                  className="rounded-t-lg px-1"
+                />
+              </div>
+
               {/* Product Details */}
-              <div className="p-4">
-                <h3 className="text-sm dark:text-white font-semibold text-gray-900 truncate">
+              <div className="p-4 max-md:p-2 max-sm:p-1">
+                <h3 className="text-sm dark:text-white font-semibold text-gray-900 truncate max-md:text-xs max-sm:text-[0.7rem]">
                   {product.name}
                 </h3>
-                <p className="text-xs text-gray-500 mt-1 truncate">
+                <p className="text-xs text-gray-500 mt-1 truncate max-md:text-[0.8rem] max-sm:text-[0.6rem]">
                   {product.description}
                 </p>
-                <p className="text-purple-500 font-bold mt-2">
+                <p className="text-purple-500 font-bold mt-2 max-md:mt-1 max-sm:text-[0.8rem]">
                   {product.price}
                 </p>
-                <button className="bg-btnGray font-bold flex justify-center items-center mx-auto dark:bg-white dark:text-black text-white mt-2 w-[4rem] py-1 rounded-full text-sm hover:bg-purple-600">
+                <button className="bg-btnGray font-bold flex justify-center items-center mx-auto dark:bg-white dark:text-black text-white mt-2 w-[4rem] py-1 rounded-full text-sm hover:bg-purple-600 max-md:w-[3rem] max-md:py-0.5 max-sm:w-[2.5rem] max-sm:py-0.5 max-sm:text-[0.6rem]">
                   Buy
                 </button>
               </div>
@@ -86,19 +92,10 @@ const PopularItemSection: React.FC<SectionProps> = ({
           ))}
         </div>
 
-        {/* Navigation Arrows - Centered below Cards */}
-        <div className="flex justify-center gap-6 mt-6 lg:justify-end">
-          <button className="bg-gray-100 dark:bg-[#161328] rounded-full w-11 h-11 border-gray-200 hover:bg-gray-300">
-            <Image
-              src="/images/arrowLeft.png"
-              alt="Left Arrow"
-              width={20}
-              height={25}
-              className="mx-auto"
-            />
-          </button>
+        <div className="flex justify-end  gap-6 mt-6 lg:justify-end">
+          
           <button
-            className="bg-custom-gradient rounded-full w-11 h-11 hover:bg-purple-700"
+            className="bg-custom-gradient rounded-full w-10 h-10 hover:bg-purple-700 mr-5"
             onClick={handleExplore}
           >
             <Image
@@ -106,12 +103,15 @@ const PopularItemSection: React.FC<SectionProps> = ({
               alt="Right Arrow"
               width={20}
               height={25}
-              className="mx-auto"
+              className="mx-auto "
             />
           </button>
         </div>
       </div>
+
     </div>
+   
+
   );
 };
 
