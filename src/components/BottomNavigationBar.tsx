a9183eba16c6ed18787c566f4b129f838839a120
Aylaimran-02 <aylaimran02@gmail.com>
2025-01-17 22:42:08 +0500

    ..

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import ThemeToggle from "./ThemeToggle";
-import { useSelector } from "react-redux";
+"use client";
 import { RootState } from "@/app/Store/Store";
-import { dark } from "@mui/material/styles/createPalette";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import ThemeToggle from "./ThemeToggle";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const BottomNavigationBar = () => {
+  const isLogin = useSelector((state: RootState) => state.login.isLoggedIn);
   const theme = useSelector((state: RootState) => state.Theme.theme);
   const [backgroundColor, setBackgroundColor] = useState<string>();
   const [isScrolling, setIsScrolling] = useState(false);
@@ -47,24 +49,24 @@ const BottomNavigationBar = () => {
 
   return (
     <div className={`flex justify-evenly items-center h-20  dark:bg-[#0D0D12]`}>
-      {/* <div> */}
-      <Image
-        src="/images/gameIcon.png"
-        alt="logo-img"
-        width={100}
-        height={100}
-        className="max-sm:w-[4rem] md:w-[2rem] md:ml-[0.2rem] lg:w-[5rem] md:h-[1.8rem] lg:h-12 max-sm:mx-auto"
-      />
+      <Link href="/">
+        <Image
+          src="/images/gameIcon.png"
+          alt="logo-img"
+          width={100}
+          height={100}
+          className="max-sm:w-[4rem] md:w-[2rem] md:ml-[0.2rem] lg:w-[5rem] md:h-[1.8rem] lg:h-12 max-sm:mx-auto"
+        />
+      </Link>
       {/* </div> */}
-      <div className="hidden md:flex md:gap-5 md:pl-2 lg:gap-[2rem] font-bold md:text-[0.6rem] lg:text-[0.8rem] whitespace-nowrap text-navTextLight dark:text-white">
-        <Link href="#">Laptops</Link>
-        <Link href="#">Desktops</Link>
-        <Link href="#">Store</Link>
-        <Link href="#">Components</Link>
-        <Link href="#">Blogs</Link>
-        <Link href="#">About Us</Link>
-        <Link href="#">Contact Us</Link>
-        <Link href="#" className="text-secondaryColorLight">
+      <div className="hidden md:flex md:gap-5 md:pl-2 lg:gap-[2rem] font-bold md:text-[0.6rem]  lg:text-[0.8rem] whitespace-nowrap text-navTextLight dark:text-white">
+        <Link href="/desktop">Desktops</Link>
+        <Link href="/laptops">Laptops</Link>
+        <Link href="/console-screen">Store</Link>
+        <Link href="/usedparts">Components</Link>
+        <Link href="/console-screen">Blogs</Link>
+        <Link href="/contact">Contact Us</Link>
+        <Link href="/details" className="text-secondaryColorLight">
           Inspection
         </Link>
       </div>
@@ -88,7 +90,7 @@ const BottomNavigationBar = () => {
         {/* Drawer Menu */}
         {isDrawerOpen && (
           <div
-            className={`flex-nowrap fixed left-0 bg-white dark:bg-black dark:text-white w-[10rem] h-screen z-50 bg-red flex flex-col items-center space-y-6 overflow-y-auto
+            className={`flex-nowrap fixed left-0  dark:bg-black bg-white dark:text-white w-[10rem] h-[85vh] z-50 bg-red flex flex-col items-center space-y-6 max-sm:gap-1 max-sm:space-y-2  overflow-y-auto
                             ${
                               isDrawerOpen
                                 ? "animate-slide-in"
@@ -105,21 +107,21 @@ const BottomNavigationBar = () => {
               className="rounded-full"
             />
             <p className="text-secondaryColorLight">Ayla Imran</p>
-
             <Link
-              href="#"
-              className=" text-lg hover:text-gray-300 "
-              onClick={() => setIsDrawerOpen(false)}
-            >
-              Laptops
-            </Link>
-            <Link
-              href="#"
+              href="/desktop"
               className="text-lg hover:text-gray-300"
               onClick={() => setIsDrawerOpen(false)}
             >
               Desktops
             </Link>
+            <Link
+              href="/laptops"
+              className=" text-lg  hover:text-gray-300 "
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Laptops
+            </Link>
+
             <Link
               href="#"
               className="text-lg hover:text-gray-300"
@@ -128,7 +130,7 @@ const BottomNavigationBar = () => {
               Store
             </Link>
             <Link
-              href="#"
+              href="/usedparts"
               className="text-lg hover:text-gray-300"
               onClick={() => setIsDrawerOpen(false)}
             >
@@ -141,22 +143,16 @@ const BottomNavigationBar = () => {
             >
               Blogs
             </Link>
+            
             <Link
-              href="#"
-              className="text-lg hover:text-gray-300"
-              onClick={() => setIsDrawerOpen(false)}
-            >
-              About Us
-            </Link>
-            <Link
-              href="#"
+              href="/contact"
               className=" text-lg hover:text-gray-300"
               onClick={() => setIsDrawerOpen(false)}
             >
               Contact Us
             </Link>
             <Link
-              href="#"
+              href="/details"
               className=" text-lg hover:text-gray-300"
               onClick={() => setIsDrawerOpen(false)}
             >
@@ -164,16 +160,18 @@ const BottomNavigationBar = () => {
             </Link>
 
             <div className="flex-col items-center mx-auto pl-0">
-              <div className="w-[7rem] mx-auto h-10 bg-custom-gradient rounded-full flex justify-center items-center gap-2">
-                <Image
-                  src="/images/btnIcon.png"
-                  className="w-[0.8rem]"
-                  width={18}
-                  height={18}
-                  alt="btnIcon"
-                />
-                <p className="text-sm text-white">Post Your Ad</p>
-              </div>
+              <Link href="/postadd">
+                <div className="w-[7rem] mx-auto h-10 bg-custom-gradient rounded-full flex justify-center items-center gap-2 cursor-pointer">
+                  <Image
+                    src="/images/btnIcon.png"
+                    className="w-[0.8rem]"
+                    width={18}
+                    height={18}
+                    alt="btnIcon"
+                  />
+                  <span className="text-sm text-white">Post Your Ad</span>
+                </div>
+              </Link>
 
               <ThemeToggle />
             </div>
@@ -181,29 +179,47 @@ const BottomNavigationBar = () => {
         )}
       </div>
       <div className="hidden md:flex md:justify-between items-center md:gap-[0.9rem]">
-        <div className="md:w-[5rem] lg:max-w-[30rem] lg:min-w-[8rem] lg:ml-2 md:h-6 lg:h-10 md:ml-[0.1rem]  bg-custom-gradient rounded-full flex justify-center items-center gap-2">
+        <Link href="/postadd">
+          <div className="md:w-[5rem] lg:max-w-[30rem] lg:min-w-[8rem] lg:ml-2 md:h-6 lg:h-10 md:ml-[0.1rem] bg-custom-gradient rounded-full flex justify-center items-center gap-2 cursor-pointer">
+            <Image
+              src="/images/btnIcon.png"
+              className="md:w-[0.6rem]"
+              width={18}
+              height={18}
+              alt="btnIcon"
+            />
+            <p className="md:text-[0.5rem] lg:text-[0.7rem] font-bold text-white">
+              Post Your Ad
+            </p>
+          </div>
+        </Link>
+
+        {isLogin ? (
           <Image
-            src="/images/btnIcon.png"
-            className="md:w-[0.6rem]"
-            width={18}
-            height={18}
-            alt="btnIcon"
+            src="/images/profile.png"
+            alt="Profile"
+            width={40}
+            height={40}
+            className="rounded-full md:w-[1rem] lg:w-[1.8rem] md:mx-0"
           />
-          <p className="md:text-[0.5rem] lg:text-[0.7rem] font-bold text-white">
-            Post Your Ad
-          </p>
-        </div>
-        {/* <div className="flex space-x-4 items-center"> */}
-        <Image
-          src="/images/profile.png"
-          alt="Profile"
-          width={40}
-          height={40}
-          className="rounded-full md:w-[1rem] lg:w-[1.8rem] md:mx-0"
-        />
+        ) : (
+          <Link href="/Auth/login">
+            <div className="md:w-[5rem] lg:max-w-[30rem] lg:min-w-[8rem] lg:ml-2 md:h-6 lg:h-10 md:ml-[0.1rem] bg-custom-gradient rounded-full flex justify-center items-center gap-2 cursor-pointer">
+              <Image
+                src="/images/btnIcon.png"
+                className="md:w-[0.6rem]"
+                width={18}
+                height={18}
+                alt="btnIcon"
+              />
+              <p className="md:text-[0.5rem] lg:text-[0.7rem] font-bold text-white">
+                Login
+              </p>
+            </div>
+          </Link>
+        )}
         <ThemeToggle />
       </div>
-      {/* </div> */}
     </div>
   );
 };
