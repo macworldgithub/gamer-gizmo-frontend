7b49d0dce0bf5e1f7307960666e4382ae7935dee
Aylaimran-02 <aylaimran02@gmail.com>
2025-01-17 22:31:14 +0500

    ..

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -1,19 +1,17 @@
-"use client";
-import { RootState } from "@/app/Store/Store";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import ThemeToggle from "./ThemeToggle";
 import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/Store/Store";
+import { dark } from "@mui/material/styles/createPalette";
 
 const BottomNavigationBar = () => {
-  const isLogin = useSelector((state: RootState) => state.login.isLoggedIn);
   const theme = useSelector((state: RootState) => state.Theme.theme);
   const [backgroundColor, setBackgroundColor] = useState<string>();
   const [isScrolling, setIsScrolling] = useState(false);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [firstClick, setFirstClick] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   useEffect(() => {
     if (theme === "day") {
@@ -47,109 +45,165 @@ const BottomNavigationBar = () => {
     setFirstClick(true);
   };
 
-  // const onMouseEnter = () => {
-  //   setIsDropdownOpen(true);
-  // };
-
-  // const onMouseout = () => {
-  //   setIsDropdownOpen(false);
-  // };
-
   return (
     <div className={`flex justify-evenly items-center h-20  dark:bg-[#0D0D12]`}>
-      <Link href="/">
-        <Image
-          src="/images/gameIcon.png"
-          alt="logo-img"
-          width={100}
-          height={100}
-          className="max-sm:w-[4rem] md:w-[2rem] md:ml-[0.2rem] lg:w-[5rem] md:h-[1.8rem] lg:h-12 max-sm:mx-auto"
-        />
-      </Link>
-
-      <div className="hidden md:flex md:gap-5 md:pl-2 lg:gap-[2rem] font-bold md:text-[0.6rem]  lg:text-[0.8rem] whitespace-nowrap text-navTextLight dark:text-white">
-        <Link href="/desktop">Desktops</Link>
-        <Link href="/laptops">Laptops</Link>
-        <Link href="/console-screen">Store</Link>
-        <Link href="/usedparts">Components</Link>
-        <Link href="/console-screen">Blogs</Link>
-        <Link href="/contact">Contact Us</Link>
-        <Link href="/details" className="text-secondaryColorLight">
+      {/* <div> */}
+      <Image
+        src="/images/gameIcon.png"
+        alt="logo-img"
+        width={100}
+        height={100}
+        className="max-sm:w-[4rem] md:w-[2rem] md:ml-[0.2rem] lg:w-[5rem] md:h-[1.8rem] lg:h-12 max-sm:mx-auto"
+      />
+      {/* </div> */}
+      <div className="hidden md:flex md:gap-5 md:pl-2 lg:gap-[2rem] font-bold md:text-[0.6rem] lg:text-[0.8rem] whitespace-nowrap text-navTextLight dark:text-white">
+        <Link href="#">Laptops</Link>
+        <Link href="#">Desktops</Link>
+        <Link href="#">Store</Link>
+        <Link href="#">Components</Link>
+        <Link href="#">Blogs</Link>
+        <Link href="#">About Us</Link>
+        <Link href="#">Contact Us</Link>
+        <Link href="#" className="text-secondaryColorLight">
           Inspection
         </Link>
       </div>
 
-      <div className="hidden md:flex md:justify-between items-center md:gap-[0.9rem]">
-        {/* Post Your Ad Button */}
-        <Link href="/postadd">
-          <div className="md:w-[5rem] lg:max-w-[30rem] lg:min-w-[8rem] lg:ml-2 md:h-6 lg:h-10 md:ml-[0.1rem] bg-custom-gradient rounded-full flex justify-center items-center gap-2 cursor-pointer">
+      <div className="md:hidden absolute left-0 ml-4 ">
+        <button
+          onClick={toggleDrawer}
+          className="focus:outline-none text-black dark:text-white"
+        >
+          <div className="w-[1.6rem] h-[1.1rem]">
             <Image
-              src="/images/btnIcon.png"
-              className="md:w-[0.6rem]"
-              width={18}
-              height={18}
-              alt="btnIcon"
+              src="/images/drawerIcon.svg"
+              alt="Drawer Icon"
+              width={35}
+              height={45}
+              className="dark:invert"
             />
-            <p className="md:text-[0.5rem] lg:text-[0.7rem] font-bold text-white">
-              Post Your Ad
-            </p>
           </div>
-        </Link>
+        </button>
 
-        {/* Profile and Dropdown */}
-        {isLogin ? (
+        {/* Drawer Menu */}
+        {isDrawerOpen && (
           <div
-            className="relative"
-           
+            className={`flex-nowrap fixed left-0 bg-white dark:bg-black dark:text-white w-[10rem] h-screen z-50 bg-red flex flex-col items-center space-y-6 overflow-y-auto
+                            ${
+                              isDrawerOpen
+                                ? "animate-slide-in"
+                                : firstClick
+                                ? "animate-slide-out"
+                                : "hidden"
+                            }`}
           >
             <Image
               src="/images/profile.png"
               alt="Profile"
               width={40}
               height={40}
-              className="rounded-full md:w-[1rem] lg:w-[1.8rem] md:mx-0 cursor-pointer"
-              onClick={() =>{setIsDropdownOpen(!isDropdownOpen)}}
+              className="rounded-full"
             />
-            {isDropdownOpen && (
-              <div
-                className="absolute top-[120%] left-1/2 transform -translate-x-1/2 bg-white shadow-lg rounded-md w-40 mt-2 z-50"
-              >
-                <ul className="py-2 text-sm text-black grid justify-center underline ">
-                  <li className="px-4 py-2 hover:bg-gray-100">
-                    <Link href="/profile">Profile</Link>
-                  </li>
-                  <li className="px-4 py-2 hover:bg-gray-100">
-                    <Link href="/card">Card</Link>
-                  </li>
-                  <li className="px-4 py-2 hover:bg-gray-100">
-                    <Link href="/favorites">Favorite</Link>
-                  </li>
-                  <li className="px-4 py-2 hover:bg-gray-100">
-                    <Link href="/orders">Order</Link>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-        ) : (
-          <Link href="/Auth/login">
-            <div className="md:w-[5rem] lg:max-w-[30rem] lg:min-w-[8rem] lg:ml-2 md:h-6 lg:h-10 md:ml-[0.1rem] bg-custom-gradient rounded-full flex justify-center items-center gap-2 cursor-pointer">
-              <Image
-                src="/images/btnIcon.png"
-                className="md:w-[0.6rem]"
-                width={18}
-                height={18}
-                alt="btnIcon"
-              />
-              <p className="md:text-[0.5rem] lg:text-[0.7rem] font-bold text-white">
-                Login
-              </p>
-            </div>
-          </Link>
-        )}
+            <p className="text-secondaryColorLight">Ayla Imran</p>
 
+            <Link
+              href="#"
+              className=" text-lg hover:text-gray-300 "
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Laptops
+            </Link>
+            <Link
+              href="#"
+              className="text-lg hover:text-gray-300"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Desktops
+            </Link>
+            <Link
+              href="#"
+              className="text-lg hover:text-gray-300"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Store
+            </Link>
+            <Link
+              href="#"
+              className="text-lg hover:text-gray-300"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Components
+            </Link>
+            <Link
+              href="#"
+              className="text-lg hover:text-gray-300"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Blogs
+            </Link>
+            <Link
+              href="#"
+              className="text-lg hover:text-gray-300"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              About Us
+            </Link>
+            <Link
+              href="#"
+              className=" text-lg hover:text-gray-300"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Contact Us
+            </Link>
+            <Link
+              href="#"
+              className=" text-lg hover:text-gray-300"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Inspection
+            </Link>
+
+            <div className="flex-col items-center mx-auto pl-0">
+              <div className="w-[7rem] mx-auto h-10 bg-custom-gradient rounded-full flex justify-center items-center gap-2">
+                <Image
+                  src="/images/btnIcon.png"
+                  className="w-[0.8rem]"
+                  width={18}
+                  height={18}
+                  alt="btnIcon"
+                />
+                <p className="text-sm text-white">Post Your Ad</p>
+              </div>
+
+              <ThemeToggle />
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="hidden md:flex md:justify-between items-center md:gap-[0.9rem]">
+        <div className="md:w-[5rem] lg:max-w-[30rem] lg:min-w-[8rem] lg:ml-2 md:h-6 lg:h-10 md:ml-[0.1rem]  bg-custom-gradient rounded-full flex justify-center items-center gap-2">
+          <Image
+            src="/images/btnIcon.png"
+            className="md:w-[0.6rem]"
+            width={18}
+            height={18}
+            alt="btnIcon"
+          />
+          <p className="md:text-[0.5rem] lg:text-[0.7rem] font-bold text-white">
+            Post Your Ad
+          </p>
+        </div>
+        {/* <div className="flex space-x-4 items-center"> */}
+        <Image
+          src="/images/profile.png"
+          alt="Profile"
+          width={40}
+          height={40}
+          className="rounded-full md:w-[1rem] lg:w-[1.8rem] md:mx-0"
+        />
         <ThemeToggle />
       </div>
+      {/* </div> */}
     </div>
   );
 };
