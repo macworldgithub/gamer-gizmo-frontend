da99ed034f9efed16f9c7a3a8c369f5a5d2b4cef
ayla <ayla@macworldtechnology.com>
2025-01-23 17:58:02 +0500

    ..

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Loader from "@/components/Loader";
+import { useRouter } from "next/navigation";
 
 interface Category {
   id: number;
@@ -16,7 +17,13 @@ const Page: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
 
   const defaultIcon = "/images/pcParts.jpg";
-  const categoryUrl = `${process.env.NEXT}`;
+
+  const router = useRouter();
+  const categoryMapping: Record<string, { path: string; icon: string }> = {
+    Desktops: { path: "/", icon: "/images/desktopImage.jpg" },
+    Laptops: { path: "/laptops", icon: "/images/LaptopImage.png" },
+    Components: { path: "/", icon: "/images/components.jpg" },
+  };
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -27,7 +34,10 @@ const Page: React.FC = () => {
         if (response.data && response.data.data) {
           const fetchedCategories = response.data.data.map((category: any) => ({
             ...category,
-            icon: category.icon || defaultIcon,
+            icon:
+              category.icon ||
+              categoryMapping[category.name]?.icon ||
+              "/images/default.jpg",
           }));
           setCategories(fetchedCategories);
         } else {
@@ -44,12 +54,20 @@ const Page: React.FC = () => {
     fetchCategories();
   }, []);
 
+  const handleCategoryClick = (categoryName: string) => {
+    const targetCategory = categoryMapping[categoryName] || {
+      path: "/",
+      icon: "/images/default.jpg",
+    };
+    router.push(targetCategory.path);
+  };
+
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="w-full mx-auto px-4 py-8 dark:text-white bg-white dark:bg-secondaryBlack">
       <h1 className="text-center max-md:text-lg md:text-2xl font-bold">
         Hello, what are you listing today?
       </h1>
-      <p className="text-center text-gray-500 max-md:text-sm mt-2">
+      <p className="text-center text-gray-500 dark:text-white max-md:text-sm mt-2">
         Select the area that best suits your ad
       </p>
 
@@ -58,24 +76,27 @@ const Page: React.FC = () => {
       ) : error ? (
         <p className="text-center text-red-500 mt-4">{error}</p>
       ) : categories.length === 0 ? (
-        <p className="text-center text-gray-500 mt-4">No categories found.</p>
+        <p className="text-center text-gray-500 mt-4 dark:text-white">
+          No categories found.
+        </p>
       ) : (
-        <div className="md:w-[50rem] max-md:w-[20rem] mx-auto grid max-md:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
+        <div className="md:w-[50rem] max-md:w-[20rem] mx-auto grid max-md:grid-cols-2 md:grid-cols-3  gap-4 mt-8">
           {categories.map((category) => (
             <div
               key={category.id}
-              className="flex flex-col items-center justify-center border hover:bg-gray-200 border-gray-200 rounded-lg shadow-md p-6 hover:shadow-lg cursor-pointer transition duration-200"
+              className="flex flex-col items-center justify-center border  hover:bg-gray-200 dark:hover:bg-gray-400  border-gray-200 rounded-lg shadow-md p-6 hover:shadow-lg cursor-pointer transition duration-200"
+              onClick={() => handleCategoryClick(category.name)}
             >
-              <div className="relative max-md:w-16 max-md:h-16 md:w-32 md:h-32 max-md:mb-0 md:mb-4">
+              <div className="relative  max-md:w-16 max-md:h-16 md:w-32 md:h-32  max-md:mb-0 md:mb-4 ">
                 <Image
                   src={category.icon}
                   alt={category.name}
                   layout="fill"
                   objectFit="contain"
-                  className="rounded-lg"
+                  className="rounded-lg bg-none bg-opacity-0"
                 />
               </div>
-              <h2 className="md:text-lg max-md:text-sm font-medium text-gray-800 max-md:mt-0 md:mt-4 truncate">
+              <h2 className="md:text-lg max-md:text-sm font-medium text-gray-800 max-md:mt-0 md:mt-4 dark:text-white truncate">
                 {category.name}
               </h2>
             </div>
