9f5ddc7ebeb41a4e7bdbcffc0880997484a392e5
Muhammad Ahmed <m.ahmed.fahim02@gmail.com>
2025-01-26 20:07:04 +0500

    Add to cart

---
+++ src/app/Add_to_cart/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShoppingCart from './ShoppingCart'
+
+const page = () => {
+  return (
+    <div>
+      <ShoppingCart/>
+    </div>
+  )
+}
+
+export default page
