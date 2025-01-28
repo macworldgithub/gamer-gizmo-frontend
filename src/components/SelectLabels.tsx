5993a1e4c91b6895fc076d6edc4a6997da5cd323
ayla <ayla@macworldtechnology.com>
2025-01-28 11:45:40 +0500

    filter section fixed

--- src/components/SelectLabels.tsx
+++ src/components/SelectLabels.tsx
@@ -53,8 +53,8 @@ export default function SelectLabels() {
 
   return (
     <Wrapper>
-      <div className="flex max-sm:flex-col w-[100%]  h-max p-1 rounded bg-white justify-center items-center">
-        <div className="flex max-sm:flex-col max-sm:mb-2 justify-center mb-1 max-lg:gap-4 lg:gap-1 w-[100%]  h-max items-center ">
+      <div className="flex max-sm:flex-col w-[100%] bg-white h-max p-1 rounded bg-w justify-center items-center">
+        <div className="flex max-sm:flex-col max-sm:mb-2 justify-center lg:justify-around xl:justify-around  mb-1 max-lg:gap-4 lg:gap-4 w-[100%]  h-max items-center ">
           {dropdownOptions.map((dropdown, index) => (
             <div key={index} className="flex justify-center items-center">
               <Select
@@ -101,7 +101,7 @@ export default function SelectLabels() {
                   fontFamily: "Urbanist",
                   fontWeight: "600",
                   ".MuiSelect-icon": {
-                    color: "#6345ed", // Change the arrow color here
+                    color: "#6345ed",
                   },
                 }}
               >
@@ -128,7 +128,7 @@ export default function SelectLabels() {
           ))}
         </div>
 
-        <div className="bg-custom-gradient  lg:w-[180px] md:w-[100px] max-sm:w-60 sm:w-[80px] h-[40px] lg:h-12  rounded-full  flex justify-center items-center md:text-base font-medium text-white">
+        <div className="bg-custom-gradient  lg:w-[150px] md:w-[100px] md:ml-2 max-sm:w-60 sm:w-[80px] h-[40px] lg:h-12  rounded-full  flex justify-center items-center md:text-base font-medium text-white">
           Filter
         </div>
       </div>
