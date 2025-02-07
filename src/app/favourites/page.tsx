5930ca065d506e7a28378abcddadaa17b929e20e
Talal <talalzafar84@gmail.com>
2025-02-07 16:15:26 +0500

    .

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+"use client";
+import React, { useLayoutEffect } from "react";
 import FavoritesPage from "./FavoritesPage";
 import MainPage from "./MainPage";
 import Wrapper from "@/components/Common/Wrapper/Wrapper";
+import { RootState } from "@/components/Store/Store";
+import { useSelector } from "react-redux";
+
+import { redirect } from "next/navigation";
 const page = () => {
+  const token = useSelector((state: RootState) => state.user.token);
+
+  useLayoutEffect(() => {
+    if (!token) {
+      return redirect("/auth/login");
+    }
+  }, []);
   return (
     // <Wrapper>
     <div className="flex w-[100%] ">
