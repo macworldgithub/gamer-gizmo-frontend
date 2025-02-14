e5702abf89a337172f3520f65497341cb7b6ee58
Talal <talalzafar84@gmail.com>
2025-02-14 17:09:46 +0500

    .

--- src/app/blogs/ItemCard.tsx
+++ src/app/blogs/ItemCard.tsx
@@ -1,129 +1,119 @@
+"use client";
+import CustomLoader from "@/components/CustomLoader";
+import axios from "axios";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { formatDate } from "../utils/formatDate";
 
-const tagColors = {
-  Product: "bg-blue-100 text-blue-600",
-  Research: "bg-indigo-100 text-indigo-600",
-  Frameworks: "bg-orange-100 text-orange-600",
-  Leadership: "bg-green-100 text-green-600",
-  Management: "bg-teal-100 text-teal-600",
-  Design: "bg-pink-100 text-pink-600",
-  Tools: "bg-gray-100 text-gray-600",
-  Saas: "bg-yellow-100 text-yellow-600",
-  Podcast: "bg-red-100 text-red-600",
-  "Customer Success": "bg-purple-100 text-purple-600",
-};
-
-const blogPosts = [
-  {
-    id: 1,
-    author: "Alec Whitten",
-    date: "1 Jan 2023",
-    title: "Bill Walsh leadership lessons",
-    description:
-      "Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?",
-    tags: ["Leadership", "Management"],
-    image: "/images/weather.png",
-  },
-  {
-    id: 2,
-    author: "Demi Wilkinson",
-    date: "1 Jan 2023",
-    title: "PM mental models",
-    description:
-      "Mental models are simple expressions of complex processes or relationships winning Dynasty?.",
-    tags: ["Product", "Research", "Frameworks"],
-    image: "/images/meeting.png",
-  },
-  {
-    id: 3,
-    author: "Candice Wu",
-    date: "1 Jan 2023",
-    title: "What is Wireframing?",
-    description:
-      "Introduction to Wireframing and its Principles. Learn from the best in the industry.",
-    tags: ["Design", "Research"],
-    image: "/images/computer.png",
-  },
-  {
-    id: 4,
-    author: "Natali Craig • ",
-    date: "1 Jan 2023",
-    title: "How collaboration makes us better designers",
-    description:
-      "Collaboration can make our teams stronger, and our individual designs better.",
-    tags: ["Design", "Research"],
-    image: "/images/collaboration.png",
-  },
-  {
-    id: 5,
-    author: "Drew Cano •  ",
-    date: "1 Jan 2023",
-    title: "Our top 10 Javascript frameworks to use designers",
-    description:
-      "JavaScript frameworks make development easy with extensive features and functionalities.",
-    tags: ["softwareDevelopment", "Tools", "Saas"],
-    image: "/images/java.png",
-  },
-  {
-    id: 6,
-    author: "Orlando Diggs • ",
-    date: "1 Jan 2023",
-    title: "Podcast: Creating a better CX Community",
-    description:
-      "Starting a community doesn’t need to be complicated, but how do you get started?",
-    tags: ["Podcast", "Customer Success"],
-    image: "/images/podcast.png",
-  },
+const tagColors = [
+  "bg-blue-100 text-blue-600",
+  "bg-indigo-100 text-indigo-600",
+  "bg-orange-100 text-orange-600",
+  "bg-green-100 text-green-600",
+  "bg-teal-100 text-teal-600",
+  "bg-pink-100 text-pink-600",
+  "bg-gray-100 text-gray-600",
+  "bg-yellow-100 text-yellow-600",
+  "bg-red-100 text-red-600",
+  "bg-purple-100 text-purple-600",
 ];
 
 export default function BlogCards() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchBlogs = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/blogs/getAll`
+      );
+      const blogs = response.data.data.map((blog: any, index: number) => ({
+        key: index,
+        Created_at: blog.created_at,
+        blogId: blog.id,
+        image: blog.images,
+        title: blog.title,
+        description: blog.content,
+        tags: blog.tags,
+      }));
+
+      setData(blogs);
+    } catch (error) {
+      toast.error("Failed to fetch blog data!");
+      console.error("Error fetching blogs:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const getRandomTagColor = () =>
+    tagColors[Math.floor(Math.random() * tagColors.length)];
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+  const truncateHtml = (html: string, limit: number) => {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = html; // Parse HTML
+    let text = tempDiv.textContent || tempDiv.innerText || ""; // Extract plain text
+    return text.length > limit ? text.substring(0, limit) + "..." : text;
+  };
   return (
     <div className="container mx-auto p-3 sm:p-4 md:p-6">
       <h2 className="text-xl sm:text-2xl font-bold mb-6 sm:mb-8 lg:text-center">
         All Blog Posts
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-md:grid-cols-2 gap-3 sm:gap-4 md:gap-6 md:max-w-6xl mx-auto">
-        {blogPosts.map((post) => (
-          <div
-            key={post.id}
-            className="bg-white dark:bg-black shadow-lg overflow-hidden w-full max-w-xs sm:max-w-xs md:max-w-xs mx-auto lg:max-w-xl  "
-          >
-            <Image
-              src={post.image}
-              alt={post.title}
-              width={400}
-              height={250}
-              className="w-full h-36 sm:h-40 md:h-36 object-cover"
-            />
-            <div className="p-2 sm:p-3 md:p-4">
-              <p className="text-xs sm:text-sm md:text-xs text-purple-400 lg:text-lg">
-                {post.author} • {post.date}
-              </p>
-              <h3 className="text-sm sm:text-xs md:text-xs font-semibold mt-2 mb-1 lg:text-sm dark:text-white">
-                {post.title}
-              </h3>
-              <p className="text-xs sm:text-xs md:text-xs text-gray-700 mb-2 lg:text-sm dark:text-white">
-                {post.description}
-              </p>
-              <div className="flex flex-wrap gap-1 sm:gap-2">
-                {post.tags?.map((tag, index) => (
-                  <span
-                    key={index}
-                    className={`px-2 py-1 rounded font-semibold 
-        text-[10px] sm:text-[7px] md:text-[10px] lg:text-sm  
-        px-1 sm:px-1.5 md:px-1.5 lg:px-2 
-        py-0.5 sm:py-1 md:py-0.5 lg:py-1
-        ${tagColors[tag] || "bg-gray-200 text-gray-700"} 
+        {data &&
+          data.length > 0 &&
+          data.map((post: any) => (
+            <div
+              key={post.key}
+              className="bg-white dark:bg-black shadow-lg overflow-hidden w-full max-w-xs sm:max-w-xs md:max-w-xs mx-auto lg:max-w-xl  "
+            >
+              {post.image && (
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${post.image}`}
+                  alt={post.title}
+                  width={400}
+                  height={250}
+                  className="w-full h-36 sm:h-40 md:h-36 object-cover"
+                />
+              )}
+              <div className="p-2 sm:p-3 md:p-4">
+                <p className="text-xs sm:text-sm md:text-xs text-purple-400 lg:text-lg">
+                  Gamer Gizmo • {formatDate(post.Created_at)}
+                </p>
+                <h3 className="text-sm sm:text-xs md:text-xs font-semibold mt-2 mb-1 lg:text-sm dark:text-white">
+                  {post.title}
+                </h3>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: truncateHtml(post.description, 100),
+                  }}
+                  className="text-xs sm:text-xs md:text-xs text-gray-700 mb-2 lg:text-sm dark:text-white"
+                />
+                <div className="flex flex-wrap gap-1 sm:gap-2">
+                  {/* @ts-expect-error khjh jk */}
+                  {post.tags.split(",").map((tag, index) => (
+                    <span
+                      key={index}
+                      className={`px-2 py-1 rounded font-semibold 
+                      text-[10px] sm:text-[7px] md:text-[10px] lg:text-sm  
+                      px-1 sm:px-1.5 md:px-1.5 lg:px-2 
+                      py-0.5 sm:py-1 md:py-0.5 lg:py-1
+          ${getRandomTagColor()}
       `}
-                  >
-                    {tag}
-                  </span>
-                ))}
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
+      {loading && <CustomLoader />}
     </div>
   );
 }
--- src/app/blogs/UsBlogs.tsx
+++ src/app/blogs/UsBlogs.tsx
@@ -1,146 +1,235 @@
+"use client";
+import CustomLoader from "@/components/CustomLoader";
+import axios from "axios";
 import Image from "next/image";
-
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { formatDate } from "../utils/formatDate";
 export default function UsBlogs() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const tagColors = [
+    "bg-blue-100 text-blue-600",
+    "bg-indigo-100 text-indigo-600",
+    "bg-orange-100 text-orange-600",
+    "bg-green-100 text-green-600",
+    "bg-teal-100 text-teal-600",
+    "bg-pink-100 text-pink-600",
+    "bg-gray-100 text-gray-600",
+    "bg-yellow-100 text-yellow-600",
+    "bg-red-100 text-red-600",
+    "bg-purple-100 text-purple-600",
+  ];
+  const fetchBlogs = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/blogs/getRecentsBlogs`
+      );
+      const blogs = response.data.data.map((blog: any, index: number) => ({
+        key: index,
+        Created_at: blog.created_at,
+        blogId: blog.id,
+        image: blog.images,
+        title: blog.title,
+        description: blog.content,
+        tags: blog.tags,
+      }));
+
+      setData(blogs);
+    } catch (error) {
+      toast.error("Failed to fetch blog data!");
+      console.error("Error fetching blogs:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const getRandomTagColor = () =>
+    tagColors[Math.floor(Math.random() * tagColors.length)];
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+  const truncateHtml = (html: string, limit: number) => {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = html; // Parse HTML
+    let text = tempDiv.textContent || tempDiv.innerText || ""; // Extract plain text
+    return text.length > limit ? text.substring(0, limit) + "..." : text;
+  };
   return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="max-w-[1200px] mx-auto p-6">
       <h2 className="text-3xl font-bold mb-8 dark:text-white">
         Recent blog posts
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="lg:col-span-2 space-y-4">
-          <Image
-            src="/images/UX.png"
-            alt="Office setup"
-            width={800}
-            height={400}
-            className="w-full object-cover"
-          />
-          <div>
-            <p className="text-sm text-purple-600 font-semibold">
-              Olivia Rhye • 1 Jan 2023
-            </p>
-            <h3 className="text-2xl font-bold mt-1 hover:underline cursor-pointer max-md:text-sm dark:text-white">
-              UX review presentations
-            </h3>
-            <p className="text-gray-600 mt-2 dark:text-white">
-              How do you create compelling presentations that wow your
-              colleagues and impress your managers?
-            </p>
-            <div className="flex space-x-2 mt-3">
-              <span className="bg-purple-100 text-purple-600 px-3 py-1 rounded-full text-xs font-medium">
-                Design
-              </span>
-              <span className="bg-blue-100 text-blue-600 px-3 py-1 rounded-full text-xs font-medium">
-                Research
-              </span>
-              <span className="bg-pink-100 text-pink-600 px-3 py-1 rounded-full text-xs font-medium">
-                Presentation
-              </span>
+        {data[0] && (
+          <div className="lg:col-span-2 space-y-4">
+            <Image
+              // @ts-expect-error jh jk
+              src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${data[0].image}`}
+              alt="Office setup"
+              width={800}
+              height={400}
+              className="w-full object-cover"
+            />
+            <div>
+              <p className="text-sm text-purple-600 font-semibold">
+                {/* @ts-expect-error jk hk */}
+                Gamer Gizmo • {formatDate(data[0].Created_at)}
+              </p>
+              <h3 className="text-2xl font-bold mt-1 hover:underline cursor-pointer max-md:text-sm dark:text-white">
+                {/* @ts-expect-error jsdb */}
+                {data[0].title}
+              </h3>
+              <p
+                dangerouslySetInnerHTML={{
+                  //  @ts-expect-error jsdb
+                  __html: truncateHtml(data[0].description, 100),
+                }}
+                className="text-gray-600 mt-2 dark:text-white"
+              />
+              <div className="flex space-x-2 mt-3">
+                {/* @ts-expect-error bhj kbj */}
+                {data[0].tags.split(",").map((tag, index) => (
+                  <span
+                    className={`bg-purple-100 text-purple-600 px-3 py-1 rounded-full text-xs font-medium  ${getRandomTagColor()}`}
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        )}
 
         {/* Post 2 */}
         <div className="space-y-6">
-          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-            <Image
-              src="/images/leon.png"
-              alt="Team working"
-              width={200}
-              height={120}
-              className="w-full md:w-60 h-48 object-cover "
-            />
-            <div>
+          {data[1] && (
+            <div className="flex flex-col  md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+              <Image
+                // @ts-expect-error jh jk
+                src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${data[1].image}`}
+                alt="Team working"
+                width={200}
+                height={120}
+                className="w-full md:w-40 h-40 object-cover "
+              />
+              <div>
+                <p className="text-xs text-purple-600 font-semibold">
+                  {/* @ts-expect-error jk hk */}
+                  Gamer Gizmo • {formatDate(data[1].Created_at)}
+                </p>
+
+                <h3 className="text-lg font-bold mt-1 hover:underline cursor-pointer max-md:text-sm dark:text-white">
+                  {/* @ts-expect-error jsdb */}
+                  {data[1].title.slice(0, 10)}...
+                </h3>
+                <p
+                  dangerouslySetInnerHTML={{
+                    //  @ts-expect-error jsdb
+                    __html: truncateHtml(data[1].description, 40),
+                  }}
+                  className="text-gray-600 mt-2 text-sm line-clamp-4 dark:text-white"
+                />
+
+                <div className="flex space-x-2 mt-2">
+                  {/* @ts-expect-error bhj kbj */}
+                  {data[1].tags.split(",").map((tag, index) => (
+                    <span
+                      className={`bg-purple-100 text-purple-600 px-3 py-1 rounded-full text-xs font-medium  ${getRandomTagColor()}`}
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+          {data[2] && (
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+              <Image
+                // @ts-expect-error jk jk
+                src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${data[2].image}`}
+                alt="Workspace"
+                width={200}
+                height={100}
+                className="w-full md:w-40 h-40 object-cover"
+              />
+              <div>
+                <p className="text-xs text-purple-600 font-semibold">
+                  {/* @ts-expect-error jk hk */}
+                  Gamer Gizmo • {formatDate(data[2].Created_at)}
+                </p>
+                <h3 className="text-lg font-bold mt-1 hover:underline cursor-pointer max-md:text-sm dark:text-white">
+                  {/* @ts-expect-error jsdb */}
+                  {data[2].title.slice(0, 10)}...
+                </h3>
+                <p
+                  dangerouslySetInnerHTML={{
+                    //  @ts-expect-error jsdb
+                    __html: truncateHtml(data[2].description, 40),
+                  }}
+                  className="text-gray-600 mt-2 text-sm line-clamp-4 dark:text-white"
+                />
+                <div className="flex space-x-2 mt-2">
+                  {/* @ts-expect-error bhj kbj */}
+                  {data[2].tags.split(",").map((tag, index) => (
+                    <span
+                      className={`bg-purple-100 text-purple-600 px-3 py-1 rounded-full text-xs font-medium  ${getRandomTagColor()}`}
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {data[3] && (
+          <div className="lg:col-span-12 flex flex-col lg:flex-row gap-6">
+            <div className="w-full lg:w-1/2">
+              <Image
+                // @ts-expect-error jk jk
+                src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${data[3].image}`}
+                alt="Climate Change"
+                width={800}
+                height={400}
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <div className="flex flex-col justify-center space-y-3 lg:w-1/2">
               <p className="text-xs text-purple-600 font-semibold">
-                Phoenix Baker • 1 Jan 2023
+                {/* @ts-expect-error jk hk */}
+                Gamer Gizmo • {formatDate(data[3].Created_at)}
               </p>
-              <h3 className="text-sm font-bold mt-1 hover:underline cursor-pointer dark:text-white">
-                Migrating to Linear 101
+              <h3 className="text-2xl font-bold hover:underline cursor-pointer max-md:text-sm dark:text-white">
+                {/* @ts-expect-error jkh kj */}
+                {data[3].title.slice(0, 100)}...
               </h3>
-              <p className="text-gray-600 mt-2 text-sm line-clamp-4 dark:text-white">
-                Linear helps streamline software projects, sprints, tasks, and
-                bug tracking. Here’s how to get...
-              </p>
+              <p
+                dangerouslySetInnerHTML={{
+                  //  @ts-expect-error jsdb
+                  __html: truncateHtml(data[3].description, 100),
+                }}
+                className="text-gray-600 mt-2 text-sm line-clamp-4 dark:text-white"
+              />
               <div className="flex space-x-2 mt-2">
-                <span className="bg-purple-100 text-purple-600 px-3 py-1 rounded-full text-xs font-medium">
-                  Design
-                </span>
-                <span className="bg-pink-100 text-pink-600 px-3 py-1 rounded-full text-xs font-medium">
-                  Research
-                </span>
+                {/* @ts-expect-error bhj kbj */}
+                {data[3].tags.split(",").map((tag, index) => (
+                  <span
+                    className={`bg-purple-100 text-purple-600 px-3 py-1 rounded-full text-xs font-medium  ${getRandomTagColor()}`}
+                  >
+                    {tag}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
-
-          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-            <Image
-              src="/images/lana.png"
-              alt="Workspace"
-              width={200}
-              height={120}
-              className="w-full md:w-60 h-48 object-cover"
-            />
-            <div>
-              <p className="text-xs text-purple-600 font-semibold">
-                Lana Steiner • 1 Jan 2023
-              </p>
-              <h3 className="text-sm font-bold mt-1 hover:underline cursor-pointer dark:text-white">
-                Building your API Stack
-              </h3>
-              <p className="text-gray-600 mt-2 text-sm line-clamp-4 dark:text-white">
-                The rise of RESTful APIs has been met by a rise in tools for
-                creating, testing, and managing APIs efficiently...
-              </p>
-              <div className="flex space-x-2 mt-2">
-                <span className="bg-green-100 text-green-600 px-3 py-1 rounded-full text-xs font-medium">
-                  Design
-                </span>
-                <span className="bg-pink-100 text-pink-600 px-3 py-1 rounded-full text-xs font-medium">
-                  Research
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="lg:col-span-12 flex flex-col lg:flex-row gap-6">
-          <div className="w-full lg:w-1/2">
-            <Image
-              src="/images/climate.png"
-              alt="Climate Change"
-              width={800}
-              height={400}
-              className="w-full h-full object-cover"
-            />
-          </div>
-
-          <div className="flex flex-col justify-center space-y-3 lg:w-1/2">
-            <p className="text-sm text-purple-600 font-semibold">
-              Olivia Rhye • 1 Jan 2023
-            </p>
-            <h3 className="text-2xl font-bold hover:underline cursor-pointer max-md:text-sm dark:text-white">
-              Grid system for better Design User Interface
-            </h3>
-            <p className="text-gray-600 md:w-xl dark:text-white ">
-              A grid system is a design tool used to arrange content on a
-              webpage. It is a series of vertical and horizontal lines that
-              create a matrix of intersecting points, which can be used to align
-              and organize page elements. Grid systems are used to create a
-              consistent look and feel across a website, and can help to make
-              the layout more visually appealing and easier to navigate.
-            </p>
-            <div className="flex space-x-2 mt-2">
-              <span className="bg-purple-100 text-purple-600 px-3 py-1 rounded-full text-xs font-medium">
-                Design
-              </span>
-              <span className="bg-pink-100 text-pink-600 px-3 py-1 rounded-full text-xs font-medium">
-                Interface
-              </span>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
+      {loading && <CustomLoader />}
     </div>
   );
 }
