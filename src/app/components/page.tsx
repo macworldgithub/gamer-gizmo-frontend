47f181f745601a5f1c7717ffc3c819cf0e589efd
ayla <ayla@macworldtechnology.com>
2025-02-17 18:04:12 +0500

    ...

---
+++ src/app/components/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import PageHeader from "@/components/PageHeader";
+import React from "react";
+import HeroSection from "./HeroSection";
+import { useSearchParams } from "next/navigation";
+const Components = () => {
+  const params = useSearchParams();
+
+  // Define an object with the possible query parameters
+  const queryParams = [
+    "processor",
+    "storage",
+    "location",
+    "condition",
+    "gpu",
+    "ram",
+    "price",
+  ];
+
+  // Initialize an object to store the parameters
+  const queryObject = {};
+
+  // Loop through the queryParams array and extract each one from URL search params
+  queryParams.forEach((param) => {
+    const value = params.get(param);
+    // @ts-expect-error
+    queryObject[param] = value ? value : "";
+  });
+
+  return (
+    <div className="w-full">
+      <PageHeader pageName="Components" title="Components" />
+      <HeroSection query={queryObject} />
+    </div>
+  );
+};
+
+export default Components;
