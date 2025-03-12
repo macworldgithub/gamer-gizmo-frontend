0d7b353710c6ce20f7cd5126a59691d3a3f51b1e
ayla <ayla@macworldtechnology.com>
2025-03-12 10:22:08 +0500

    ..

--- src/app/product-details/[id]/Rightsection.tsx
+++ src/app/product-details/[id]/Rightsection.tsx
@@ -8,8 +8,11 @@ import { MdOutlineMailLock } from "react-icons/md";
 import { WhatsAppOutlined } from "@ant-design/icons";
 import { MdVerified } from "react-icons/md";
 import Verified from "../../../../public/images/Verified.svg";
+import { useSelector } from "react-redux";
+import { RootState } from "@/components/Store/Store";
 
 const Rightsection = ({ data }: any) => {
+  const token = useSelector((state: RootState) => state.user.token);
   const [quantity, setQuantity] = useState(2);
   const profileUrl = data?.users?.profile
     ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/${data?.users?.profile}`
@@ -70,103 +73,94 @@ const Rightsection = ({ data }: any) => {
       </div>
 
       {/* Seller Details Section */}
-      <div className="dark:bg-black  border-gray-300 rounded-lg p-6 w-72 h-fit bg-white shadow-md flex flex-col items-center">
-        <div className="flex justify-center gap-2 items-center">
-          <div className=" flex items-center  justify-center flex-col gap-1">
-            {data?.is_verified_by_admin ? (
-              <>
-                <MdVerified />
-                <p className="text-purple-600 font-bold text-[0.6rem]">
-                  Verified
+      <div className="dark:bg-black border-gray-300 rounded-lg p-6 w-72 h-fit bg-white shadow-md flex flex-col items-center">
+        {token ? (
+          <>
+            <div className="flex justify-center gap-2 items-center">
+              <div className="flex items-center justify-center flex-col gap-1">
+                {data?.is_verified_by_admin ? (
+                  <>
+                    <MdVerified />
+                    <p className="text-purple-600 font-bold text-[0.6rem]">
+                      Verified
+                    </p>
+                  </>
+                ) : (
+                  <p className="text-red-500 font-bold text-[0.6rem]">
+                    Not Verified
+                  </p>
+                )}
+              </div>
+              <h2 className="text-center text-lg font-bold text-gray-800 dark:text-white">
+                Seller Details
+              </h2>
+            </div>
+
+            {/* Horizontal Line */}
+            <hr className="my-4 border-gray-300 w-full" />
+
+            {/* Seller Information */}
+            <div className="flex items-center mb-6 gap-2">
+              {/* Seller Image */}
+              {profileUrl ? (
+                <img
+                  src={profileUrl}
+                  alt="User Avatar"
+                  className="w-11 h-11 rounded-full"
+                  onError={(e) => {
+                    //@ts-ignore
+                    e.target.onerror = null;
+                    //@ts-ignore
+                    e.target.style.display = "none";
+                  }}
+                />
+              ) : (
+                <FaUser className="w-8 h-8 text-gray-500" />
+              )}
+
+              {/* Seller Name and Member Since */}
+              <div className="text-left">
+                <p className="text-gray-800 font-semibold dark:text-white">
+                  {data?.users?.first_name} {data?.users?.last_name} (
+                  {data?.users?.gender})
                 </p>
-              </>
-            ) : (
-              <p className="text-red-500 font-bold text-[0.6rem]">
-                Not Verified
-              </p>
-            )}
-          </div>
-          <h2 className="text-center text-lg font-bold text-gray-800 dark:text-white">
-            Seller Details
-          </h2>
-        </div>
+                <p className="text-gray-500 text-sm dark:text-[#969696]">
+                  Member Since {formatDate(data?.users?.created_at)}
+                </p>
+              </div>
+            </div>
 
-        {/* Horizontal Line */}
-        <hr className="my-4 border-gray-300 w-full" />
+            {/* Contact Information */}
+            <div className="flex flex-col justify-center gap-2 items-center space-x-6 mb-6 dark:invert">
+              <a
+                href={`https://wa.me/${data?.users?.phone}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-4 text-black cursor-pointer"
+              >
+                <WhatsAppOutlined />
+                <div className="text-sm">{data?.users?.phone}</div>
+              </a>
 
-        {/* Seller Information */}
-        <div className="flex items-center mb-6 gap-2">
-          {/* Seller Image */}
-          {/* <Image
-            src={
-              data?.users?.profile
-                ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/${data?.users?.profile}`
-                : "/images/person.png"
-            }
-            alt="kk"
-            width={40}
-            height={40}
-            className="rounded-full mr-4"
-          /> */}
-          {profileUrl ? (
-            <img
-              src={profileUrl}
-              alt="User Avatar"
-              className="w-11 h-11    rounded-full"
-              onError={(e) => {
-                //@ts-ignore
-                e.target.onerror = null;
-                //@ts-ignore
-
-                e.target.style.display = "none";
-              }}
-            />
-          ) : (
-            <FaUser className="w-8 h-8 text-gray-500" />
-          )}
-          {/* Seller Name and Member Since */}
-          <div className="text-left">
-            <p className="text-gray-800 font-semibold dark:text-white">
-              {data?.users?.first_name} {data?.users?.last_name} ({" "}
-              {data?.users?.gender})
-            </p>
-            <p className="text-gray-500 text-sm dark:text-[#969696]">
-              Member Since {formatDate(data?.users?.created_at)}
-            </p>
-          </div>
-        </div>
-        <div className="text-sm text-black"></div>
-        {/* Icons Row */}
-        <div className="flex flex-col justify-center gap-2 items-center space-x-6 mb-6 dark:invert">
-          <a
-            href={`https://wa.me/${data?.users?.phone}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex gap-4 text-black cursor-pointer"
-          >
-            <WhatsAppOutlined />
-            <div className="text-sm">{data?.users?.phone}</div>
-          </a>
-
-          {/* Email Link */}
-          <a
-            href={`mailto:${data?.users?.email}`}
-            className="flex gap-4 text-black cursor-pointer"
-          >
-            <MdOutlineMailLock />
-            <div className="text-sm">{data?.users?.email}</div>
-          </a>
-        </div>
-
-        {/* Footer Text */}
-        {/* <p className="text-center text-sm text-gray-600 mb-2 dark:text-white">
-          See if your friends know this seller
-        </p>
-        <p className="text-center text-sm text-blue-600 font-semibold cursor-pointer hover:underline">
-          Connect with Facebook
-        </p> */}
+              <a
+                href={`mailto:${data?.users?.email}`}
+                className="flex gap-4 text-black cursor-pointer"
+              >
+                <MdOutlineMailLock />
+                <div className="text-sm">{data?.users?.email}</div>
+              </a>
+            </div>
+          </>
+        ) : (
+          <p className="text-center text-gray-800 dark:text-white">
+            Please{" "}
+            <span className="text-blue-600 font-semibold cursor-pointer">
+              log in
+            </span>{" "}
+            to view seller details.
+          </p>
+        )}
       </div>
-
       {/* Third Card (Buy From GamerGizmo) */}
       <div className="dark:bg-black border-gray-300 rounded-lg p-6 w-64 h-64 bg-white shadow-md flex flex-col items-center">
         <Image
