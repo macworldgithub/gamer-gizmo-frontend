aad268f55d8f0a49734547ef2c103a7b64a6af70
shweeta jadhwani <shweetajadhwani@gmail.com>
2025-04-07 18:04:53 +0500

    ,,

--- src/components/GetStartedBadge.tsx
+++ src/components/GetStartedBadge.tsx
@@ -8,11 +8,11 @@ const GetStartedBadge = () => {
   return (
     <div>
       {/* {!profileData.is_admin_verified && ( */}
-      <div className="bg-bluishBorder p-4 rounded-lg pb-8 flex flex-row max-md:gap-2 md:gap-4 w-full justify-center items-center px-8">
+      <div className="bg-bluishBorder p-4 rounded-lg pb-8 flex flex-row max-md:gap-2 md:gap-4 w-3/4 justify-center items-center px-8">
         <div className="flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6 text-purple-500 mr-2"
+            className="w-8 h-5 text-purple-500 mr-2"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={2}
@@ -24,22 +24,22 @@ const GetStartedBadge = () => {
               d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
-          <p className="text-purple-500 font-medium md:text-lg max-md:text-[0.4rem]">
+          <p className="text-purple-500 font-small md:text-lg max-md:text-[0.2rem]">
             Got a verified badge yet?
           </p>
         </div>
-        <p className="md:text-sm max-md:text-[0.4rem] text-black ">
+        <p className="md:text-sm max-md:text-[0.3rem] text-black ">
           Get more visibility and enhance your credibility.
         </p>
         {/* {!profileData.applied_for_verification ? ( */}
         <button
           onClick={() => setOpenNicModal(true)}
-          className=" bg-purple-500 text-white max-md:px-1 max-md:text-[0.6rem] max-md:w-36 px-4 py-2 max-md:text-xs rounded-md md:text-sm hover:bg-purple-600"
+          className=" bg-purple-500 text-white max-md:px-1 max-md:text-[0.4rem] max-md:w-36 px-4 py-2 max-md:text-xs rounded-md md:text-sm hover:bg-purple-600"
         >
           Get Started
         </button>
         {/* ) : ( */}
-        <p className="text-black font-bold md:text-md max-md:text-[0.4rem]">
+        <p className="text-black font-bold md:text-sm max-md:text-[0.2rem]">
           Applied for verification, Wait for admin to verify
         </p>
         {/* )} */}
